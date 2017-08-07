93b477537376fcbb835f0e86e9ca649ad70e640d
Sandro Koch <layornos@gmail.com>
2017-08-07 07:54:15 +0200

    init commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D ball;
+    public bool isGrounded;
+    public Vector2 jump;
+    public float jumpForce = 2.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		jump = new Vector3(0, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+	    {
+	        ball.AddForce(jump * jumpForce, ForceMode2D.Impulse);
+	        isGrounded = false;
+	    }
+	}
+}
